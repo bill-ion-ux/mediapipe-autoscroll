faead6a107832853f9c5eb501e9625eb5d79671e
iamnabil <23001886@siswa.um.edu.my>
2025-11-01 23:49:53 +0800

    add hand tracking

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import time
 import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    #display the image
-    cv2.imshow('Hand Tracking', frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+with mp_hands.Hands(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+        print(results)
+        #display the image
+        cv2.imshow('Hand Tracking', frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 cap.release()
 cv2.destroyAllWindows()
 # model_path ='C:/Users/USER/mediapipe-autoscroll/gesture_recognizer.task'
