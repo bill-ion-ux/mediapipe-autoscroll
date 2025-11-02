e9ed165b7e2b1087fa2c6cb2c61e85ecbde12cc9
iamnabil <23001886@siswa.um.edu.my>
2025-11-02 23:14:34 +0800

    add points coordinate in hand landmarks

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer =  mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as hands:
@@ -17,8 +22,23 @@ with mp_hands.Hands(min_detection_confidence = 0.5, min_tracking_confidence = 0.
         image.flags.writeable = True
         image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
         print(results)
+        
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for num,hand in enumerate(hand_landmarks.landmark):
+                    h,w,c = image.shape
+                    cx,cy = int(hand.x * w), int(hand.y * h)
+                    print(f"landmark{num}: {cx},{cy}")
+                    mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+
+
+                
+
+
         #display the image
-        cv2.imshow('Hand Tracking', frame)
+        cv2.imshow('Hand Tracking', image)
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 cap.release()
