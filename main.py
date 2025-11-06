5c68a1b50233620532d3f4f8e59f4ffa5c59f26e
iamnabil <23001886@siswa.um.edu.my>
2025-11-06 18:39:31 +0800

    feature: gesture detection

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as hands:
+    def get_angle(a,b,c):
+        vecBA_x = a.x*w - b.x*w
+        vecBA_y = a.y*h - b.y*h
+        vecBC_x = c.x*w - b.x*w
+        vecBC_y = c.y*h - b.y*h
+
+        mag_BA = math.sqrt(vecBA_x**2 + vecBA_y**2)
+        mag_BC = math.sqrt(vecBC_x**2 + vecBC_y**2)
+
+        dot_product = (vecBA_x * vecBC_x) + (vecBA_y * vecBC_y) 
+
+        if mag_BA == 0 or mag_BC == 0:
+            return 0
+        
+        cos_theta = dot_product / (mag_BC * mag_BA)
+        cos_theta = max(-1.0, min(1.0, cos_theta))
+        angle_rad = math.acos(cos_theta)
+        angle_deg = angle_rad * (180 / math.pi)
+        return angle_deg
     while cap.isOpened():
         ret, frame = cap.read()
         image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
@@ -31,8 +50,32 @@ with mp_hands.Hands(min_detection_confidence = 0.5, min_tracking_confidence = 0.
                 y_max = 0
                 x_min = w
                 y_min = h
-
+                pointing_up = False
+                pointing_down = False
+                angle_index_pip = get_angle(hand_landmarks.landmark[5],hand_landmarks.landmark[6],hand_landmarks.landmark[7])
                 
+                
+                is_index_straight = angle_index_pip > 160
+                is_middle_bent = get_angle(hand_landmarks.landmark[9],hand_landmarks.landmark[10],hand_landmarks.landmark[12]) < 100
+                is_ring_bent = get_angle(hand_landmarks.landmark[13],hand_landmarks.landmark[14],hand_landmarks.landmark[16]) < 100
+                is_pinky_bent = get_angle(hand_landmarks.landmark[17],hand_landmarks.landmark[18],hand_landmarks.landmark[20]) < 100  
+                
+                is_pointing_up_direction = hand_landmarks.landmark[8].y < hand_landmarks.landmark[5].y
+                is_pointing_down_direction = hand_landmarks.landmark[8].y > hand_landmarks.landmark[5].y
+                all_others_bend = is_middle_bent and is_pinky_bent and is_ring_bent
+                if all_others_bend and is_index_straight:
+                    if is_pointing_up_direction:
+                        pointing_up = True
+                        gesture_text = "Pointing UP"
+                    elif is_pointing_down_direction:
+                        pointing_down = True
+                        gesture_text = "Pointing DOWN"
+                    else:
+                        gesture_text = "none"
+
+
+
+                 
             
                 for num,hand in enumerate(hand_landmarks.landmark):
                     cx,cy = int(hand.x * w), int(hand.y * h)
@@ -50,6 +93,12 @@ with mp_hands.Hands(min_detection_confidence = 0.5, min_tracking_confidence = 0.
                             (x_min - padding, y_min - padding), 
                             (x_max + padding, y_max + padding), 
                             (0, 255, 0), 2)
+                cv2.putText(image, gesture_text, 
+                    (x_min - padding, y_min - padding - 10), # Position (10px above the box)
+                    cv2.FONT_HERSHEY_SIMPLEX, # Font
+                    0.9, # Font scale
+                    (0, 255, 0), # Color (Green)
+                    2) # Thickness
                 mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
                  
@@ -64,22 +113,3 @@ cv2.destroyAllWindows()
 # model_path ='C:/Users/USER/mediapipe-autoscroll/gesture_recognizer.task'
 # base_options = BaseOptions(model_asset_path=model_path)
 
-def get_angle(a,b,c):
-    vecBA_x = a.x - b.x
-    vecBA_y = a.y - b.x
-    vecBC_x = c.x - b.x
-    vecBC_y = c.y - c.x
-
-    mag_BA = math.sqrt(vecBA_x**2 + vecBA_y**2)
-    mag_BC = math.sqrt(vecBC_x**2 + vecBC_y**2)
-
-    dot_product = (vecBC_x * vecBC_x) + (vecBA_y * vecBC_y)
-
-    if mag_BA == 0 or mag_BC == 0:
-        return 0
-    
-    cos_theta = max(-1.0, min(1.0, cos_theta))
-    cos_theta = dot_product / (mag_BC * mag_BA)
-    angle_rad = math.acos(cos_theta)
-    angle_deg = math.degrees(angle_rad)
-    return angle_deg
